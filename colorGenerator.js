63cd469d43861e2a1365190f16e17c612d150a6f
Tuitl Kazi <70696457+Tutil-Kazi@users.noreply.github.com>
2022-08-23 00:57:29 +0600

    Add files via upload

---
+++ colorGenerator.js
@@ -0,0 +1,16 @@
+let numEl = document.getElementById("numel")
+
+function colorGenerator(){
+    // let hexNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'e', 'f']
+    let hexNum = '0123456789abcdefghijklmnopqrstuvwxyz'
+    let color = '#'
+
+    for(let i = 0; i < 6; i++){
+        let num = Math.round(Math.random()*19)
+        let num2 = hexNum[num]
+        color += num2
+    }
+    document.getElementById("container").style.backgroundColor = color
+    numEl.style.color = color
+    numEl.innerHTML = color
+}
